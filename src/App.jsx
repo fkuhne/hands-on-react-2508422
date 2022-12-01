5108060f80d994c6581d9a80082efeb666c062fd
Felipe Kühne <fkuhne@gmail.com>
2022-12-01 01:18:56 +0000

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
+import React, { Component } from "react";
+
+// A component with React Hooks
+const Welcome = (props) => {
+  return (
+    <h1>
+      Meet the <i>{props.name}</i>
+    </h1>
+  );
+};
+
 function App() {
-  const name = 'StarGazers'
+  const name = "StarGazers";
   return (
     <div className="container">
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
+          <Welcome name="StarsGazersss" />
+          <p>
+            Members of an <b>intergalactic alliance</b>
+            <br />
+            paving the way for peace and benevolence among all species. They are
+            known for their enthusiasm for science, for their love of fun, and
+            their dedication to education.
+          </p>
+          <button className="outline" onClick={() => alert("Hi there")}>
+            Click Me
+          </button>
         </hgroup>
       </article>
     </div>
-  )
+  );
 }
-export default App
+export default App;
