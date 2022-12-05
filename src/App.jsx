a8aef55fac662c5684777a982755c52c07f5215d
Felipe Kühne <fkuhne@gmail.com>
2022-12-05 02:58:37 +0000

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,37 @@ function App() {
 
   return (
     <div className="container">
-      <article>
-        <hgroup>
-          <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the StarGazers</h1>
-          <p>
-            Members of an <b>intergalactic alliance</b>
-            paving the way for peace and benevolence among all species. They are
-            known for their enthusiasm for science, for their love of fun, and
-            their dedication to education.
-          </p>
-          <ListCast />
-        </hgroup>
-      </article>
+      <hgroup>
+        <img src="images/group.svg" alt="StarGazers Group" />
+        <h1>Meet the StarGazers</h1>
+        <p>
+          Members of an <b>intergalactic alliance</b> paving the way for peace
+          and benevolence among all species. They are known for their enthusiasm
+          for science, for their love of fun, and their dedication to education.
+        </p>
+        <ListCast
+          onChoice={(info) => {
+            setMemberInfo(info);
+          }}
+        />
+        {memberInfo && (
+          <article>
+            <hgroup>
+              <div style={{ display: "flex", gap: "1rem" }}>
+                <img
+                  style={{ width: "200px" }}
+                  src={`images/${memberInfo.slug}.svg`}
+                  alt={memberInfo.name}
+                />
+                <hgroup>
+                  <h1>{memberInfo.name}</h1>
+                  <p>{memberInfo.bio}</p>
+                </hgroup>
+              </div>
+            </hgroup>
+          </article>
+        )}
+      </hgroup>
     </div>
   );
 }
