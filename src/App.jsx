f6bcdc1e4c20dea1f37cf40081c6b161a90b4c82
Felipe Kühne <fkuhne@gmail.com>
2022-12-05 03:18:59 +0000

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import React, { Component, useState } from 'react';
-import ListCast from './components/ListCast';
+import React, { Component, useState } from "react";
+import ListCast from "./components/ListCast";
+import Modals from "./components/Modals";
 
 function App() {
-  const name = 'StarGazers'
   let [memberInfo, setMemberInfo] = useState(null);
   return (
     <div className="container">
       <hgroup>
         <img src="images/group.svg" alt="StarGazers Group" />
         <h1>Meet the Stargazers</h1>
-        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-        <ListCast onChoice={(info) => { setMemberInfo(info) }} />
-        {memberInfo &&
-          <article>
-            <hgroup>
-              <div style={{
-                display: "flex",
-                gap: '1rem'
-              }}>
-                <img style={{ width: '200px' }} src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
-                <hgroup>
-                  <h1>{memberInfo.name}</h1>
-                  <p>{memberInfo.bio}</p>
-                </hgroup>
-              </div>
-            </hgroup>
-          </article>
-        }
+        <p>
+          Members of an <b>intergalactic alliance</b> paving the way for peace
+          and benevolence among all species. They are known for their enthusiasm
+          for science, for their love of fun, and their dedication to education.
+        </p>
+        <ListCast
+          onChoice={(info) => {
+            setMemberInfo(info);
+          }}
+        />
+        {memberInfo && (
+          <Modals
+            member={memberInfo}
+            handleClose={() => {
+              setMemberInfo(null);
+            }}
+          />
+        )}
       </hgroup>
     </div>
-  )
+  );
 }
-export default App
+export default App;
